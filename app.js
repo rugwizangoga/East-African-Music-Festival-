655c597fe9ec01f577baedfb21cf53db07f3d5b5
rugwizangoga <pacific.r.iradukunda@gmail.com>
2022-09-15 04:54:05 +0100

    index.html mobile completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const navul = document.querySelector('.navul');
 const nav = document.querySelector('.navcontainer');
+const screen = window.matchMedia("(min-width: 768px)");
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
@@ -13,3 +14,11 @@ document.querySelectorAll('.navitem').forEach((n) => n.addEventListener('click',
   navul.classList.remove('active');
   nav.classList.remove('active');
 }));
+
+screen.addEventListener('change', ()=> {
+  if(screen.matches){
+    hamburger.classList.remove('active');
+    navul.classList.remove('active');
+    nav.classList.remove('active');
+  }
+});
